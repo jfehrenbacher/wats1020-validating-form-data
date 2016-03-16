8f8f9b1195c921aadf5a8be96fdbd619eddd3e4e
fehrenb1 <fehrenb1@seattleu.edu>
2016-03-16 17:04:12 -0400

    Added validation rules, tried to change messages, changed color

--- js/main.js
+++ js/main.js
@@ -6,3 +6,53 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+
+$(document).on('ready', function(){
+    // Form validation
+    $('#order-form').validate({
+        submitHandler: function(form) {
+            // If form is valid, submit it!
+            form.submit();
+        },
+        rules: {
+            "your-name": {
+                required: true,
+                maxlength: 128
+            },
+			"your=city": {
+				required: true
+			},
+            "your-state": {
+                required: true,
+                maxlength: 2
+            },
+			"your-address": {
+				required: true
+			},
+            "your-zip": {
+                required: true,
+                maxlength: 5,
+                digits: true
+            },
+            "card-holder-name": {
+                required: true,
+                maxlength: 128
+            },
+            "card-number": {
+                required: true,
+                creditcard: true
+            },
+			"expiry-month": {
+				required: true
+			},
+            "cvv": {
+                required: true,
+                maxlength: 3,
+                digits: true
+            },
+			"shipping-method": {
+			required: true
+			}
+        }
+    });
+});
